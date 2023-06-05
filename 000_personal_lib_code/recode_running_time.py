1efa602e959a11ea267d83368b3b60267270a745
SeongWook_Jang <tjddnr0912@gmail.com>
2023-06-05 12:44:56 +0900

    [230605] python coding test 3-2 to 3-3 update

---
+++ 000_personal_lib_code/recode_running_time.py
@@ -0,0 +1,6 @@
+#recomand time
+import time
+start_time = time.time() # get start time
+
+end_time = time.time() # get end time
+print("time : ", end_time - start_time) # print running time
---
+++ 001_coding_test_question/3-2.py
@@ -0,0 +1,23 @@
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+while len(data) != n:
+    print("Data length is not correct, Please input right number : ", n)
+    data = list(map(int, input().split()))
+
+data.sort()
+
+first = data[n-1]
+second = data[n-2]
+
+acc_sum = 0
+max_count = 0
+for i in range(m):
+    if max_count < k :
+        max_count += 1
+        acc_sum += first
+    else :
+        max_count = 0
+        acc_sum += second
+
+print("Acc_sum value = ", acc_sum)
